53df6ac05a90ff7e370d6a6406b6740ea8b9ceed
unaLoo <1923606858@qq.com>
2025-03-24 14:48:11 +0800

    tif-visual-test

---
+++ frontEnd/src/views/Test.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="h-full bg-white flex">
+        <div id='map' class="flex-1"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+// import * as MapOperation from '@/util/map/operation'
+import { initMap, mapManager } from '@/util/map/mapManager'
+
+onMounted(() => {
+    initMap('map')
+
+    mapManager.withMap((map) => {
+        map.addSource('test', {
+            type: 'raster',
+            tiles: [
+                'http://127.0.0.1:5000/tile/{z}/{x}/{y}.png'
+            ]
+        })
+        map.addLayer({
+            id: 'test',
+            type: 'raster',
+            source: 'test',
+            paint: {
+                'raster-opacity': 1.0,
+                'raster-color-mix': [
+                    0.1126,
+                    0.7152,
+                    0.0722,
+                    1
+                ]
+            }
+        })
+    })
+
+})
+
+
+</script>
+
+<style scoped></style>
