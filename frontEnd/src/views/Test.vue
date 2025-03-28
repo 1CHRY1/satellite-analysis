42fda2e3a8410d8bc6ff6e2de2ca263b378d58b2
unaLoo <1923606858@qq.com>
2025-03-28 20:07:08 +0800

    fix: tif_tiler

--- frontEnd/src/views/Test.vue
+++ frontEnd/src/views/Test.vue
@@ -15,7 +15,10 @@ onMounted(() => {
         map.addSource("src",{
             type:"raster",
             tiles:[
-                "http://127.0.0.1:5000/tiff1/{z}/{x}/{y}.png"
+                // no cog
+                // "http://223.2.32.242:8079/{z}/{x}/{y}.png?object=/test-images/landsat/landset7/tif/LE07_L1TP_122039_20210212_20210212_01_RT/LE07_L1TP_122039_20210212_20210212_01_RT_B1.TIF"
+               
+                "http://223.2.32.242:8079/{z}/{x}/{y}.png?object=/test-images/landset8_test/landset8_L2SP_test/tif/LC08_L2SP_118038_20241217_20241227_02_T1/LC08_L2SP_118038_20241217_20241227_02_T1_SR_B4.TIF"
             ]
         })
         map.addLayer({
@@ -25,18 +28,18 @@ onMounted(() => {
             minzoom:0,
         })
 
-        map.addSource("src2",{
-            type:"raster",
-            tiles:[
-                "http://localhost:5000/tiff2/{z}/{x}/{y}.png"
-            ]
-        })
-        map.addLayer({
-            id:"layer2",
-            type:"raster",
-            source:"src2",
-            minzoom:0,
-        })
+        // map.addSource("src2",{
+        //     type:"raster",
+        //     tiles:[
+        //         "http://223.2.32.242:8079/{z}/{x}/{y}.png?object=/test-images/landset8_test/landset8_L2SP_test/tif/LC08_L2SP_118038_20241201_20241203_02_T1/LC08_L2SP_118038_20241201_20241203_02_T1_SR_B4.TIF"
+        //     ]
+        // })
+        // map.addLayer({
+        //     id:"layer2",
+        //     type:"raster",
+        //     source:"src2",
+        //     minzoom:0,
+        // })
 
 
         window.addEventListener("keydown",(e)=>{
