62ce6dc702ff6e3c0a418befe443197d725c8b64
unaLoo <1923606858@qq.com>
2025-03-31 14:45:20 +0800

    🚀 RELEASE: NDVI Cases

--- pything/ogms_xfer/dataModel/tile.py
+++ pything/ogms_xfer/dataModel/tile.py
@@ -9,18 +9,6 @@ from ..connection.database import DatabaseClient
 class TileBase(DatabaseClient.Base):
     # __tablename__ = 'xxxx'  # 动态表名无法直接映射
     __abstract__ = True  # 这个类不会映射到数据库，只是作为基类
-    # tile_id = Column(String(36), primary_key=True, index=True)
-    image_id = Column(String(15), index=True)
-    tile_level = Column(String(36))
-    column_id = Column(Integer)
-    row_id = Column(Integer)    
-    path = Column(String(255))
-    bucket = Column(String(36))
-    cloud = Column(Float)
-    band = Column(Integer)
-    # Ignore the warning:  
-    # SAWarning: Did not recognize type 'geometry' of column 'bounding_box'
-    bounding_box = Column(Geometry(geometry_type='POLYGON'))
 
     def __repr__(self): # call when print object
         return f"Tile(tile_id={self.tile_id}, \
@@ -51,18 +39,36 @@ class TileFactory:
         # find in cache first
         if lower_scene_id in self.model_cache:
             return self.model_cache[lower_scene_id]
-        
-        # print(" create new model for ", lower_scene_id) # debug
-        
-        class DynamicTile(TileBase):
-            __tablename__ = lower_scene_id 
-            __table__ = Table(
-                lower_scene_id,
-                self.metadata,
-                Column("tile_id", String(36), primary_key=True, index=True),
-                autoload_with=self.db_engine,
-                )
+            
+        if not self.db_engine.dialect.has_table(self.db_engine.connect(), lower_scene_id):
+            raise ValueError(f"表 {lower_scene_id} 不存在于数据库中")
 
-        self.model_cache[lower_scene_id] = DynamicTile
-
-        return DynamicTile
+        # 手动定义所有列，避免使用autoload_with, autoload_with对于Geometry类型会发警告
+        table = Table(
+            lower_scene_id,
+            self.metadata,
+            Column("tile_id", String(36), primary_key=True, index=True),
+            Column("image_id", String(15), index=True),
+            Column("tile_level", String(36)),
+            Column("column_id", Integer),
+            Column("row_id", Integer),
+            Column("path", String(255)),
+            Column("bucket", String(36)),
+            Column("cloud", Float),
+            Column("band", Integer),
+            Column("bounding_box", Geometry(geometry_type='POLYGON', srid=4326))
+        )
+        
+        class_dict = {
+            '__tablename__': lower_scene_id,
+            '__table__': table
+        }
+        
+        model_cls = type(
+            f"Tile_{lower_scene_id}",
+            (TileBase,),
+            class_dict
+        )
+        
+        self.model_cache[lower_scene_id] = model_cls
+        return model_cls
