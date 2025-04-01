5b414a6d59ee416c22d3565294c9b648cfa5b2fd
unaLoo <1923606858@qq.com>
2025-04-01 10:24:39 +0800

    🐛 FIX: Docker-Version of Pything

---
+++ pything/docker_version/ogms_xfer/application/tile.py
@@ -0,0 +1,83 @@
+from ..service.tile import TileService
+from ..application.provider import Singleton
+from ..dataModel.tile import TileBase as TileDataModel
+
+class Tile:
+    
+    @staticmethod
+    def query(scene_id: str = None, image_id: str = None, cloud_range: tuple[float, float] = None, polygon: object = None, tile_ids: list[str] = None, band: int = None, tile_level: str = None):
+        tile_service: TileService = Singleton.get_instance(id="tile_service")
+        return [Tile(scene_id, tile_id) for scene_id, tile_id in tile_service.get_tiles(
+            scene_id=scene_id, 
+            image_id=image_id,
+            cloud_range=cloud_range, 
+            polygon=polygon, 
+            tile_ids=tile_ids, 
+            band=band, 
+            tile_level=tile_level)]
+
+    def __new__(cls, scene_id: str, tile_id: str):
+        """创建 Tile 实例，若 tile_id 不存在，则返回 None"""
+        tile_service: TileService = Singleton.get_instance(id="tile_service")
+        data: TileDataModel = tile_service.get_tile(scene_id, tile_id)
+        if data is None:
+            return None 
+        print('Tile new !!!!!')
+        instance = super().__new__(cls) 
+        instance._data = data
+        instance._scene_id = scene_id
+        return instance
+    
+    def __init__(self, scene_id: str, tile_id: str):
+        self._tile_service : TileService = Singleton.get_instance(id="tile_service")
+
+    @classmethod
+    def from_data_model(cls, data: TileDataModel, scene_id: str):
+        instance = super().__new__(cls) 
+        instance._data = data
+        instance._scene_id = scene_id
+        return instance
+
+    # 基础映射属性
+    @property
+    def tile_id(self) -> str:
+        return self._data.tile_id
+    
+    @property
+    def image_id(self) -> str:
+        return self._data.image_id
+    
+    @property
+    def tile_level(self) -> int:
+        return self._data.tile_level
+    
+    @property
+    def column_id(self) -> int:
+        return self._data.column_id
+    
+    @property
+    def row_id(self) -> int:
+        return self._data.row_id
+    
+    @property
+    def path(self) -> str:
+        return self._data.path
+    
+    @property
+    def bucket(self) -> str:
+        return self._data.bucket
+    
+    @property
+    def cloud(self) -> float:
+        return self._data.cloud
+    
+    @property
+    def url(self) -> str:
+        return f"/{self.bucket}/{self.path}"
+    
+    def __repr__(self):
+        return f"Tile(tile_id={self.tile_id}, image_id={self.image_id}, tile_level={self.tile_level}, column_id={self.column_id}, row_id={self.row_id}, path={self.path}, bucket={self.bucket}, cloud={self.cloud})"
+
+    # 类的个性化方法
+    def pull(self, output_path: str):
+        return self._tile_service.pull_tiles(self.image_id, self.tile_id, output_path)
---
+++ pything/docker_version/ogms_xfer/dataModel/product.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Text, String, Float, Integer
+
+from ..connection.database import DatabaseClient
+
+class Product(DatabaseClient.Base):
+    __tablename__ = 'product_table'  # map to table in database
+
+    product_id = Column(String(36), primary_key=True, index=True)
+    sensor_id = Column(String(36), index=True)
+    product_name = Column(String(255), index=True)
+    description = Column(Text)
+    resolution = Column(String(255))
+    period = Column(Integer)
+
+    def __repr__(self): # call when print object
+        return f"Product(product_id={self.product_id}, \
+            sensor_id={self.sensor_id}, \
+            product_name={self.product_name}, \
+            description={self.description}, \
+            resolution={self.resolution}, \
+            period={self.period}) \n"
