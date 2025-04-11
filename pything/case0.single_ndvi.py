0893be800b1c1332c6a47c7c0ce734d1bc0473a5
unaLoo <1923606858@qq.com>
2025-04-11 14:50:59 +0800

    case 0 searching

--- pything/case0.single_ndvi.py
+++ pything/case0.single_ndvi.py
@@ -1,15 +1,19 @@
 from ogms_xfer import OGMS_Xfer as xfer
-import datetime
+from datetime import datetime
 
 config_file_path = "config.json"
 xfer.initialize(config_file_path)
 
 ##################################################
-# 1. 检索影像
-scene = xfer.Scene("SC906772444")
+# 1. 检索影像 用ID检索
+scene = xfer.Scene("SC906772444") # 直接通过ID检索
+
 if(scene.scene_id == None):
     print("无法检索到影像, 请检查影像ID是否正确")
     exit()
+
+    
+
 
 print("检索到影像： ", scene.scene_name)
 print("波段数： ",scene.band_num)
