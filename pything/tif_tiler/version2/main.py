d1a00d96217025a51e5df564c7cfcc77016d88e7
unaLoo <1923606858@qq.com>
2025-05-22 23:40:10 +0800

    fuck

--- pything/tif_tiler/version2/main.py
+++ pything/tif_tiler/version2/main.py
@@ -1,31 +1,44 @@
-from fastapi import FastAPI
-from titiler.core.factory import TilerFactory
 
-from starlette.middleware.cors import CORSMiddleware
 
-from router import rgb, terrain, oneband
+def createApp():
+    import os
+    os.environ['PROJ_DATA'] = r'D:\env\miniconda\envs\grid\Lib\site-packages\rasterio\proj_data'
+    
+    from fastapi import FastAPI
+    from titiler.core.factory import TilerFactory
 
-app = FastAPI()
+    from starlette.middleware.cors import CORSMiddleware
 
-# Add CORS middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins (for development - be more specific in production)
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    from router import rgb, terrain, oneband
 
-# Create a TilerFactory for Cloud-Optimized GeoTIFFs
-cog = TilerFactory()
 
-# Register all the COG endpoints automatically
-app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
-app.include_router(rgb.router, tags=["RGB Composite"], prefix="/rgb")
-app.include_router(terrain.router, tags=["Terrain"], prefix="/terrain")
-app.include_router(oneband.router, tags=["One Band"], prefix="/oneband")
 
-# Optional: Add a welcome message for the root endpoint
-@app.get("/")
-def read_index():
-    return {"message": "Welcome to TiTiler"}
+    app = FastAPI()
+
+    # Add CORS middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allows all origins (for development - be more specific in production)
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    # Create a TilerFactory for Cloud-Optimized GeoTIFFs
+    cog = TilerFactory()
+
+    # Register all the COG endpoints automatically
+    app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
+    app.include_router(rgb.router, tags=["RGB Composite"], prefix="/rgb")
+    app.include_router(terrain.router, tags=["Terrain"], prefix="/terrain")
+    app.include_router(oneband.router, tags=["One Band"], prefix="/oneband")
+
+    # Optional: Add a welcome message for the root endpoint
+    @app.get("/")
+    def read_index():
+        return {"message": "Welcome to TiTiler"}
+    
+    return app
+    
+    
+app = createApp()
