af6fe45f82f3ee743c947a9f026b48a2e53b990d
Zhang Zewen <zhangzewen823@gmail.com>
2025-08-02 17:06:52 +0800

    incomplete visualization

--- frontEnd/env.config.ts
+++ frontEnd/env.config.ts
@@ -49,9 +49,9 @@ export const ENV_CONFIG: Record<string, EnvConfig> = {
         proxymap: 'http://localhost:5003',
     },
     zzw: {
-        api: 'http://192.168.1.127:8999',
-        realtime: 'http://192.168.1.127:5001',
-        websocket: 'http://192.168.1.127:8999/model/websocket',
+        api: 'http://223.2.34.8:31584',
+        realtime: 'http://223.2.34.8:5001',
+        websocket: 'http://223.2.34.8:30394/model/websocket',
         tiler: 'http://localhost:8000',
         proxymap: 'http://localhost:5003',
     }
