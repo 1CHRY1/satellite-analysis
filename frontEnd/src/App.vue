8e6358fba1c689e8703ad7386e610f9f7ffd87ec
Lingkai Shi <gislkshi@outlook.com>
2025-09-17 19:32:27 +0800

    🚀gen cube

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -2,13 +2,17 @@
 import DarkHeader from './components/layout/darkHeader.vue'
 // import Header from '@/components/layout/Header.vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { theme } from 'ant-design-vue'
+const { darkAlgorithm, defaultAlgorithm } = theme
 </script>
 
 <template>
     <div class="flex min-h-screen flex-col bg-[rgb(245,244,238)]">
         <DarkHeader class="fixed top-0 left-0 right-0 z-50 h-[56px]"></DarkHeader>
         <!-- <Header></Header> -->
-        <a-config-provider :locale="zhCN">
+        <a-config-provider :locale="zhCN" :theme="{
+            algorithm: darkAlgorithm
+        }">
             <RouterView class="relative flex-1 pt-[56px]"></RouterView>
         </a-config-provider>
         <!-- <Footer></Footer> -->
---
+++ frontEnd/src/util/map/operation/data-preparation.ts
@@ -0,0 +1,102 @@
+import { clickHandler, selectedGrid } from '@/components/dataCenter/noCloud/composables/useBox'
+import { mapManager } from '../mapManager'
+import { ezStore } from '@/store'
+import bus from '@/store/bus'
+import { watch, watchEffect } from 'vue'
+
+/**
+ * 添加网格图层，同时为初始格网图层（未填充）所用
+ * @param gridGeoJson grid的geojson
+ */
+export function map_addGridLayer(gridGeoJson: GeoJSON.FeatureCollection): void {
+    const id = 'grid-layer'
+    const fillId = id + '-fill'
+    const lineId = id + '-line'
+    const highlightId = id + '-highlight'
+    const srcId = id + '-source'
+
+    mapManager.withMap((m) => {
+        ezStore.set('map', m)
+
+        // Add a geojson source
+        m.addSource(srcId, {
+            type: 'geojson',
+            data: gridGeoJson,
+        })
+        // Add a line layer for **grid line visualization**
+        m.addLayer({
+            id: lineId,
+            type: 'line',
+            source: srcId,
+            paint: {
+                'line-color': '#F00000',
+                'line-width': 1,
+                'line-opacity': 0.3,
+            },
+        })
+        // Add a invisible fill layer for **grid picking**
+        m.addLayer({
+            id: fillId,
+            type: 'fill',
+            source: srcId,
+            paint: {
+                'fill-color': '#00FFFF',
+                'fill-opacity': ['coalesce', ['to-number', ['get', 'opacity']], 0.01],
+            },
+        })
+
+        // Add a filterable fill layer for **grid highlighting**
+        // const nowSelectedGrids = Array.from(gridStore.selectedGrids) || ['']
+        m.addLayer({
+            id: highlightId,
+            type: 'fill',
+            source: srcId,
+            paint: {
+                // 'fill-color': '#FF9900',
+                'fill-color': '#0000FF',
+                'fill-opacity': 0.3,
+            },
+            // filter: ['in', 'id', ...nowSelectedGrids],
+            filter: ['in', 'id', ''],
+        })
+
+        // 左键点击 fill 区域，更新高亮
+        m.on('click', fillId, clickHandler)
+
+        // Add a click event listener to the invisible fill layer
+        m.on('contextmenu', fillId, () => {
+            // TODO
+        })
+
+        // ezStore.set('grid-layer-cancel-watch', cancelWatch)
+        ezStore.set('grid-layer-fill-id', fillId)
+        ezStore.set('grid-layer-line-id', lineId)
+        ezStore.set('grid-layer-highlight-id', highlightId)
+        ezStore.set('grid-layer-source-id', srcId)
+    })
+}
+
+/**
+ * 删除网格图层
+ */
+export function map_destroyGridLayer(): void {
+    const gridLayer = ezStore.get('grid-layer-fill-id')
+    const gridLineLayer = ezStore.get('grid-layer-line-id')
+    const gridHighlightLayer = ezStore.get('grid-layer-highlight-id')
+    const gridSourceId = ezStore.get('grid-layer-source-id')
+    const cancelWatch = ezStore.get('grid-layer-cancel-watch')
+
+    mapManager.withMap((m) => {
+        gridLayer && m.getLayer(gridLayer) && m.off('click', gridLayer, clickHandler)
+        gridLayer && m.getLayer(gridLayer) && m.removeLayer(gridLayer)
+        gridLineLayer && m.getLayer(gridLineLayer) && m.removeLayer(gridLineLayer)
+        gridHighlightLayer && m.getLayer(gridHighlightLayer) && m.removeLayer(gridHighlightLayer)
+        gridSourceId && m.getSource(gridSourceId) && m.removeSource(gridSourceId)
+        cancelWatch && cancelWatch()
+        ezStore.delete('grid-layer-fill-id')
+        ezStore.delete('grid-layer-line-id')
+        ezStore.delete('grid-layer-highlight-id')
+        ezStore.delete('grid-layer-source-id')
+        ezStore.delete('grid-layer-cancel-watch')
+    })
+}
