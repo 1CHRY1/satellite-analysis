a248e5bcf82a5207661465b374cad4c86eb63871
Lingkai Shi <gislkshi@outlook.com>
2025-11-08 15:45:34 +0800

    🐛 tags added

--- modelServer/dataProcessing/model/methlib/schemas/method.py
+++ modelServer/dataProcessing/model/methlib/schemas/method.py
@@ -13,13 +13,11 @@ class MethodEntity:
     params: Optional[str] = None       # json 参数信息 (JSON字符串)
     execution: Optional[str] = None    # varchar(50) 执行类型 (e.g., "py", "exe", "jar")
     category: Optional[str] = None     # varchar(255) 条目
+    tags: Optional[List[str]] = None
 
     # 附加字段 (来自完整的 container_method 表，通常为描述或元数据)
     description: Optional[str] = None  # varchar(255) 描述信息
     long_desc: Optional[str] = None    # text 详细描述信息
     copyright: Optional[str] = None    # varchar(255) 版权信息
     type: Optional[str] = None         # varchar(50) 方法类型
-    input_schema: Optional[str] = None # text 输入UDXSchema
-    output_schema: Optional[str] = None# text 输出UDXSchema
-    create_user_id: Optional[int] = None # bigint(20) 创建者ID
     create_time: Optional[datetime] = None # datetime 创建时间
