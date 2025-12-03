384c1b17af9753bfb2836184023ac3092f567ebb
Lingkai Shi <gislkshi@outlook.com>
2025-12-03 19:07:58 +0800

    🚀 zh added

--- backEnd/src/main/java/nnu/mnr/satellite/model/vo/modeling/MethlibInfoVO.java
+++ backEnd/src/main/java/nnu/mnr/satellite/model/vo/modeling/MethlibInfoVO.java
@@ -21,12 +21,17 @@ public class MethlibInfoVO {
     private String name;
     private String description;
     private String longDesc;
+    private String nameZh;
+    private String descriptionZh;
+    private String longDescZh;
     private String copyright;
     private String category;
     private String uuid;
     private String type;
     @TableField(typeHandler = JSONArrayTypeHandler.class)
     private JSONArray params;
+    @TableField(typeHandler = JSONArrayTypeHandler.class)
+    private JSONArray paramsZh;
     private String execution;
     private LocalDateTime createTime;
     @TableField(typeHandler = TagIdsTypeHandler.class)
