f04619f60c1a087851cc7452ed47238f84d5afd5
luopo123 <253301116@qq.com>
2025-12-13 19:51:37 +0800

    hxf1213vector

--- backEnd/src/main/java/nnu/mnr/satellite/model/vo/resources/VectorInfoVO.java
+++ backEnd/src/main/java/nnu/mnr/satellite/model/vo/resources/VectorInfoVO.java
@@ -3,10 +3,12 @@ package nnu.mnr.satellite.model.vo.resources;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class VectorInfoVO {
     private String vectorName;
     private String tableName;
     private LocalDateTime time;
+    private List<String> fields;
 }
