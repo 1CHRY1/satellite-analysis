5e27686b19611df13d3cf0055117e51092e1c57c
Lingkai Shi <gislkshi@outlook.com>
2026-01-13 13:57:42 +0800

    🐛 bug fixes in titiler-image-visualization

--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -8,7 +8,7 @@ import { ENV_CONFIG, FIXED_CONFIG } from './env.config'
 // const ENV = process.env.NODE_ENV || 'development'
 
 
-const ENV_TARGET = 'zzw' // 一键切换配置环境，使用集群则改为cluster，使用本地则改为local
+const ENV_TARGET = 'slk' // 一键切换配置环境，使用集群则改为cluster，使用本地则改为local
 
 
 // 获取当前环境配置
