78cfc432223599632d277d502bd316687ecf8acd
luopo123 <253301116@qq.com>
2026-01-28 20:37:43 +0800

    0128_backend_pg_v3

--- backEnd/src/main/java/nnu/mnr/satellite/config/db/MybatisPlusConfig.java
+++ backEnd/src/main/java/nnu/mnr/satellite/config/db/MybatisPlusConfig.java
@@ -4,11 +4,16 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import nnu.mnr.satellite.utils.typeHandler.GeometryTypeHandler;
 import nnu.mnr.satellite.utils.typeHandler.SetTypeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -18,11 +23,29 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
  */
 
 @Configuration
+@Slf4j
 public class MybatisPlusConfig {
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(DataSource dataSource) throws SQLException {
+
+        String dbProductName = dataSource
+                .getConnection()
+                .getMetaData()
+                .getDatabaseProductName()
+                .toLowerCase();
+
+        DbType dbType;
+        if (dbProductName.contains("postgresql")) {
+            dbType = DbType.POSTGRE_SQL;
+        } else if (dbProductName.contains("mysql")) {
+            dbType = DbType.MYSQL;
+        } else if (dbProductName.contains("oracle")) {
+            dbType = DbType.ORACLE;
+        } else {
+            throw new IllegalStateException("Unsupported database: " + dbProductName);
+        }
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 根据你的数据库类型选择
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(dbType));
         return interceptor;
     }
 
